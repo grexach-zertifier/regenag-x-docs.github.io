0bba56a0a3d23bdd96fdcb7907f08a9a6f6f85c8
grexach-zertifier <grexach@zertifier.com>
2025-11-24 12:58:13 +0100

    Update footer copyright to "© Zertifier [current year]"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `© Zertifier ${new Date().getFullYear()}`,
       },
       prism: {
         theme: prismThemes.github,
